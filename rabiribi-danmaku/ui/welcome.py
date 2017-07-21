efa2dc837d25293e81eaf3fbe3d71debc5c6a1b0
Jiaming Zheng <xjmanz@gmail.com>
2017-07-21 03:19:53 -0400

    Adjust code style (without impacting major functions).

--- rabiribi-danmaku/ui/welcome.py
+++ rabiribi-danmaku/ui/welcome.py
@@ -3,6 +3,7 @@ import math
 import platform
 import random
 import sys
+import os.path
 
 from math import *
 from random import *
@@ -10,32 +11,13 @@ from pygame.locals import *
 
 class now_loading_images():
     def __init__(self):
-
         self.image = []
         self.rect = []
 
-        if platform.system() == 'Linux':
-            for i in range(10):
-                ch = "images/welcome/now_loading/now_loading_00" + str(i) + ".png"
-                self.image.append(pygame.image.load(ch).convert_alpha())
-            for i in range(10,100):
-                ch = "images/welcome/now_loading/now_loading_0" + str(i) + ".png"
-                self.image.append(pygame.image.load(ch).convert_alpha())
-            for i in range(100,360):
-                ch = "images/welcome/now_loading/now_loading_" + str(i) + ".png"
-                self.image.append(pygame.image.load(ch).convert_alpha())
-        if platform.system() == 'Windows':
-            for i in range(10):
-                ch = "images\\welcome\\now_loading\\now_loading_00" + str(i) + ".png"
-                self.image.append(pygame.image.load(ch).convert_alpha())
-            for i in range(10,100):
-                ch = "images\\welcome\\now_loading\\now_loading_0" + str(i) + ".png"
-                self.image.append(pygame.image.load(ch).convert_alpha())
-            for i in range(100,360):
-                ch = "images\\welcome\\now_loading\\now_loading_" + str(i) + ".png"
-                self.image.append(pygame.image.load(ch).convert_alpha())
-
         for i in range(360):
+            # Platform independent path with right justified file index
+            ch = os.path.join("..", "images", "welcome", "now_loading", "now_loading_" + str(i).rjust(3, '0') + ".png")
+            self.image.append(pygame.image.load(ch).convert_alpha())
             temp = self.image[i].get_rect()
             temp.top = 400
             temp.left = 350
@@ -43,85 +25,40 @@ class now_loading_images():
 
 class Wall_paper():
     def __init__(self):
-        if platform.system() == 'Linux':
-            self.wall_paper = { \
-                "Aruraune":pygame.image.load("images/welcome/wallpaper/Aruraune.jpg"), \
-                "Ashuri":pygame.image.load("images/welcome/wallpaper/Ashuri.jpg"), \
-                "Chocolate":pygame.image.load("images/welcome/wallpaper/Chocolate.jpg"), \
-                "Cicini":pygame.image.load("images/welcome/wallpaper/Cicini.jpg"), \
-                "Cocoa":pygame.image.load("images/welcome/wallpaper/Cocoa.jpg"), \
-                "Erina":pygame.image.load("images/welcome/wallpaper/Erina.jpg"), \
-                "Irisu":pygame.image.load("images/welcome/wallpaper/Irisu.jpg"), \
-                "Kotri":pygame.image.load("images/welcome/wallpaper/Kotri.jpg"), \
-                "Lilith":pygame.image.load("images/welcome/wallpaper/Lilith.jpg"), \
-                "Miriam":pygame.image.load("images/welcome/wallpaper/Miriam.jpg"), \
-                "Miru":pygame.image.load("images/welcome/wallpaper/Miru.jpg"), \
-                "Nieve":pygame.image.load("images/welcome/wallpaper/Nieve.jpg"), \
-                "Nixie":pygame.image.load("images/welcome/wallpaper/Nixie.jpg"), \
-                "Noah":pygame.image.load("images/welcome/wallpaper/Noah.jpg"), \
-                "Pandora":pygame.image.load("images/welcome/wallpaper/Pandora.jpg"), \
-                "Ribbon":pygame.image.load("images/welcome/wallpaper/Ribbon.jpg"), \
-                "Rita":pygame.image.load("images/welcome/wallpaper/Rita.jpg"), \
-                "Rumi":pygame.image.load("images/welcome/wallpaper/Rumi.jpg"), \
-                "Saya":pygame.image.load("images/welcome/wallpaper/Saya.jpg"), \
-                "Seana":pygame.image.load("images/welcome/wallpaper/Seana.jpg"), \
-                "Syaro":pygame.image.load("images/welcome/wallpaper/Syaro.jpg"), \
-                "Vanilla":pygame.image.load("images/welcome/wallpaper/Vanilla.jpg") \
-            }
-        if platform.system() == 'Windows':
-            self.wall_paper = { \
-                "Aruraune":pygame.image.load("images\\welcome\\wallpaper\\Aruraune.jpg"), \
-                "Ashuri":pygame.image.load("images\\welcome\\wallpaper\\Ashuri.jpg"), \
-                "Chocolate":pygame.image.load("images\\welcome\\wallpaper\\Chocolate.jpg"), \
-                "Cicini":pygame.image.load("images\\welcome\\wallpaper\\Cicini.jpg"), \
-                "Cocoa":pygame.image.load("images\\welcome\\wallpaper\\Cocoa.jpg"), \
-                "Erina":pygame.image.load("images\\welcome\\wallpaper\\Erina.jpg"), \
-                "Irisu":pygame.image.load("images\\welcome\\wallpaper\\Irisu.jpg"), \
-                "Kotri":pygame.image.load("images\\welcome\\wallpaper\\Kotri.jpg"), \
-                "Lilith":pygame.image.load("images\\welcome\\wallpaper\\Lilith.jpg"), \
-                "Miriam":pygame.image.load("images\\welcome\\wallpaper\\Miriam.jpg"), \
-                "Miru":pygame.image.load("images\\welcome\\wallpaper\\Miru.jpg"), \
-                "Nieve":pygame.image.load("images\\welcome\\wallpaper\\Nieve.jpg"), \
-                "Nixie":pygame.image.load("images\\welcome\\wallpaper\\Nixie.jpg"), \
-                "Noah":pygame.image.load("images\\welcome\\wallpaper\\Noah.jpg"), \
-                "Pandora":pygame.image.load("images\\welcome\\wallpaper\\Pandora.jpg"), \
-                "Ribbon":pygame.image.load("images\\welcome\\wallpaper\\Ribbon.jpg"), \
-                "Rita":pygame.image.load("images\\welcome\\wallpaper\\Rita.jpg"), \
-                "Rumi":pygame.image.load("images\\welcome\\wallpaper\\Rumi.jpg"), \
-                "Saya":pygame.image.load("images\\welcome\\wallpaper\\Saya.jpg"), \
-                "Seana":pygame.image.load("images\\welcome\\wallpaper\\Seana.jpg"), \
-                "Syaro":pygame.image.load("images\\welcome\\wallpaper\\Syaro.jpg"), \
-                "Vanilla":pygame.image.load("images\\welcome\\wallpaper\\Vanilla.jpg") \
-            }
-        self.number = {1:"Aruraune", \
-                       2:"Ashuri", \
-                       3:"Chocolate", \
-                       4:"Cicini", \
-                       5:"Cocoa", \
-                       6:"Erina", \
-                       7:"Irisu", \
-                       8:"Kotri", \
-                       9:"Lilith", \
-                       10:"Miriam", \
-                       11:"Miru", \
-                       12:"Nieve", \
-                       13:"Nixie", \
-                       14:"Noah", \
-                       15:"Pandora", \
-                       16:"Ribbon", \
-                       17:"Rita", \
-                       18:"Rumi", \
-                       19:"Saya", \
-                       20:"Seana", \
-                       21:"Syaro", \
-                       22:"Vanilla" \
-                       }
+        self.characters = ["Aruraune",
+                           "Ashuri",
+                           "Chocolate",
+                           "Cicini",
+                           "Cocoa",
+                           "Erina",
+                           "Irisu",
+                           "Kotri",
+                           "Lilith",
+                           "Miriam",
+                           "Miru",
+                           "Nieve",
+                           "Nixie",
+                           "Noah",
+                           "Pandora",
+                           "Ribbon",
+                           "Rita",
+                           "Rumi",
+                           "Saya",
+                           "Seana",
+                           "Syaro",
+                           "Vanilla"
+                           ]
 
-        for i in range(1,23):
-            self.wall_paper[self.number[i]] = pygame.transform.scale(self.wall_paper[self.number[i]], (853,480))
+        # Initialize wallpapers with list comprehension
+        self.wall_paper = [
+            pygame.image.load(os.path.join("..", "images", "welcome", "wallpaper", "{}.jpg".format(character))) for
+            character in self.characters]
+
+        # Each rect corresponds to the wall_paper at the same index
         self.rect = []
-        for i in range(1,23):
-            temp = self.wall_paper[self.number[i]].get_rect()
+        for i in range(len(self.characters)):
+            self.wall_paper[i] = pygame.transform.scale(self.wall_paper[i], (853, 480))
+            temp = self.wall_paper[i].get_rect()
             temp.left = -213
             temp.top = 0
             self.rect.append(temp)
@@ -132,7 +69,9 @@ def opening(screen):
 
     Wallpaper = Wall_paper()
     loading_image = now_loading_images()
-    randseed = randint(1,22)
+
+    # Seed is generated inclusively at the endpoints
+    randseed = randint(0, len(Wallpaper.characters) - 1)
     clock = pygame.time.Clock()
 
     while running:
@@ -141,7 +80,8 @@ def opening(screen):
                 pygame.quit()
                 sys.exit()
 
-        screen.blit(Wallpaper.wall_paper[Wallpaper.number[randseed-1]], Wallpaper.rect[randseed])
+        # Both wall_paper and rect are accessed through index
+        screen.blit(Wallpaper.wall_paper[randseed], Wallpaper.rect[randseed])
         screen.blit(loading_image.image[time_count], loading_image.rect[time_count])
         time_count += 1
         if time_count == 360:
